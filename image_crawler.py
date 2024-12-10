e8f303376913dead3f195d7adb83d8a5e1d8313f
sofuetakuma <kawahagi0620@gmail.com>
2024-12-10 12:29:12 +0900

    画像のクローラーにカスタムダウンローダーを実装

--- image_crawler.py
+++ image_crawler.py
@@ -1,13 +1,32 @@
 # ライブラリを呼び出す
 from icrawler.builtin import BingImageCrawler
+from icrawler.builtin import ImageDownloader
+from pathlib import Path
 
-# クローラーを生成、保存先などを指定（今回は'images'フォルダに指定）
-bing_crawler = BingImageCrawler(downloader_threads=4, storage={"root_dir": "images"})
+# カスタムダウンローダーを作成
+class PrefixedDownloader(ImageDownloader):
+    def get_filename(self, task, default_ext):
+        filename = super().get_filename(task, default_ext)
+        return f"{self.prefix}_{filename}"
 
-# キーワードや枚数を入力させてそれに応じて画像収集する
+# キーワードを入力
+keyword = str(input("名前を打ってね！>>>"))
+max_num = int(input("何枚集める？>>>"))
+
+# クローラーを生成、保存先などを指定
+bing_crawler = BingImageCrawler(
+    downloader_cls=PrefixedDownloader,
+    downloader_threads=4,
+    storage={"root_dir": "images"}
+)
+
+# プレフィックスを設定
+bing_crawler.downloader.prefix = keyword
+
+# 画像収集を実行
 bing_crawler.crawl(
-    keyword=str(input("名前を打ってね！>>>")),
+    keyword=keyword,
     filters=None,
     offset=0,
-    max_num=int(input("何枚集める？>>>")),
+    max_num=max_num,
 )
